a67b3b03653eb9f9e91edd3ce79961c94e5ae495
Kevin Wu <sierangho@gmail.com>
2020-09-24 16:25:33 -0700

    AMP-30054 fix javascript injection of scores from airtable

--- flappy/leaderboard-prod.js
+++ flappy/leaderboard-prod.js
@@ -1,47 +1,55 @@
 "use strict";
 
-var Airtable = require('airtable');
-var base = new Airtable({ apiKey: 'keymW1ZElKs4tF7ib' }).base('appCppypdYKJeG9QI');
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "keymW1ZElKs4tF7ib" }).base(
+  "appCppypdYKJeG9QI"
+);
 
 let counter = 0;
 
 function getHighscore() {
-    $('#highscore-content').show();
+  $("#highscore-content").show();
 
-    let title = 'Leaderboard';
-    let baseString = 'Data Explorer Scores';
-    if (counter % 2 === 1) {
-        baseString = 'Data Explorer Scores (Hard)';
-        title = 'Leaderboard (Hard)';
-    }
+  let title = "Leaderboard";
+  let baseString = "Data Explorer Scores";
+  if (counter % 2 === 1) {
+    baseString = "Data Explorer Scores (Hard)";
+    title = "Leaderboard (Hard)";
+  }
 
-    $('#hidden-list').text("");
+  $("#hidden-list").text("");
 
-    counter += 1;
+  counter += 1;
 
-    base(baseString).select({
-        fields: [ 'score', 'vehicle' ],
-        sort: [
-            { field: 'score', direction: 'desc' }
-        ],
-        maxRecords: 20
-    }).eachPage(function page(records, fetchNextPage) {
+  base(baseString)
+    .select({
+      fields: ["score", "vehicle"],
+      sort: [{ field: "score", direction: "desc" }],
+      maxRecords: 20,
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
         records.forEach(function (record) {
-            $('#hidden-list').append("<li>" + record.get('vehicle') + "<span id='score'>" + record.get('score') + "</span></li>");
+          var liNode = $("<li/>").text(record.get("vehicle"));
+          var spanNode = $("<span/>")
+            .attr("id", "score")
+            .text(record.get("score"));
+          liNode.append(spanNode);
+          $("#hidden-list").append(liNode);
         });
         fetchNextPage();
-    }, function done(error) {
+      },
+      function done(error) {
         console.log(error);
-        $("#highscore-list").text('');
-        $("#highscore-list").append($('#hidden-list').children());
+        $("#highscore-list").text("");
+        $("#highscore-list").append($("#hidden-list").children());
         $("#highscore-title").text(title);
-    });
-
+      }
+    );
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
+  getHighscore();
 
-    getHighscore();
-
-    setInterval(getHighscore, 10000);
-})
+  setInterval(getHighscore, 10000);
+});
--- flappy/leaderboard.js
+++ flappy/leaderboard.js
@@ -1,48 +1,58 @@
 "use strict";
 
-var Airtable = require('airtable');
-var base = new Airtable({ apiKey: 'keymW1ZElKs4tF7ib' }).base('appCppypdYKJeG9QI');
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "keymW1ZElKs4tF7ib" }).base(
+  "appCppypdYKJeG9QI"
+);
 
 let counter = 0;
 
 function getHighscore() {
-    $('#highscore-content').show();
+  $("#highscore-content").show();
 
-    let title = 'Leaderboard';
-    let baseString = 'Data Explorer Scores';
-    if (counter % 2 === 1) {
-        baseString = 'Data Explorer Scores (Hard)';
-        title = 'Leaderboard (Hard)';
-    }
+  let title = "Leaderboard";
+  let baseString = "Data Explorer Scores";
+  if (counter % 2 === 1) {
+    baseString = "Data Explorer Scores (Hard)";
+    title = "Leaderboard (Hard)";
+  }
 
-    $('#hidden-list').text("");
+  $("#hidden-list").text("");
 
-    counter += 1;
+  counter += 1;
 
-    base(baseString).select({
-        fields: [ 'score', 'vehicle' ],
-        sort: [
-            { field: 'score', direction: 'desc' }
-        ],
-        maxRecords: 100,
-    }).eachPage(function page(records, fetchNextPage) {
+  base(baseString)
+    .select({
+      fields: ["score", "vehicle"],
+      sort: [{ field: "score", direction: "desc" }],
+      maxRecords: 100,
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
         records.forEach(function (record) {
-            $('#hidden-list').append("<li class='list-row'><div class='name'>" + record.get('vehicle') + "</div><div class='score'>" + record.get('score') +
-                "</div></li>");
+          var liNode = $("<li/>").attr("class", "list-row").append();
+          var nameNode = $("<div/>")
+            .attr("class", "name")
+            .text(record.get("vehicle"));
+          var scoreNode = $("<div/>")
+            .attr("class", "score")
+            .text(record.get("score"));
+          liNode.append(nameNode).append(scoreNode);
+          $("#hidden-list").append(liNode);
         });
         fetchNextPage();
-    }, function done(error) {
+      },
+      function done(error) {
         console.log(error);
-        $("#highscore-list").text('');
-        $("#highscore-list").append($('#hidden-list').children());
+        $("#highscore-list").text("");
+        $("#highscore-list").append($("#hidden-list").children());
         $("#highscore-title").text(title);
-    });
-
+      }
+    );
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
+  getHighscore();
 
-    getHighscore();
-
-    setInterval(getHighscore, 10000);
-})
+  setInterval(getHighscore, 10000);
+});
